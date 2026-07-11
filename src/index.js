8078124436b400e0b34d726559da6fd867e4b56d
alfredo minchez <aminchez@uni.edu.pe>
2026-07-10 19:11:09 -0500

    all images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // src/index.js
-import "./styles.css";
-import { greeting } from "./greeting.js";
+import odinImage from "./odin.jpeg";
 
-console.log(greeting);
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
